15d706104efa1b0728f73a245aa12c9e012944df
Rafael Barros <rafa.a.barros@hotmail.com>
2021-02-27 14:34:11 -0300

    create a first page

---
+++ lib/util/customWidget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+Widget buildSvgIcon(String pathImage) => SvgPicture.asset(
+    pathImage,
+    width: 48,
+    height: 48
+);
+
+Widget divisorListMain() => Divider(
+  height: 1,
+  color: Colors.black54,
+);
